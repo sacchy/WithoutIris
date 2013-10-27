6f4f22cedb9e71ba200eb851b877abc702d14b8c
sacchy <systemtelescope@gmail.com>
2013-10-28 01:32:39 +0900

    Initial Commit

---
+++ WithoutIris/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WithoutIris
+//
+//  Created by 佐藤 昌樹 on 2013/10/28.
+//  Copyright (c) 2013年 sacchy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WithoutIris/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WithoutIris
+//
+//  Created by 佐藤 昌樹 on 2013/10/28.
+//  Copyright (c) 2013年 sacchy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
